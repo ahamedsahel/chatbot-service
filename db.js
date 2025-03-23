6d72b9baf026fbf1820e319145b36abe785a8dc4
ahamedsahel <ahamedsahel@gmail.com>
2025-03-23 15:42:25 +0400

    fix url path

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root', // Replace with your MySQL username
-  password: 'root123', // Replace with your MySQL password
-  database: 'tudu_db' // Your database name
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 db.connect((err) => {
